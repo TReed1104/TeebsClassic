40f22d5b72e103c64e69c19003e0223a9eee8325
Tom Reed <t.reed1104@gmail.com>
2019-10-14 15:32:06 +0100

    Added a rounding to decimal place function

--- TeebsClassic.lua
+++ TeebsClassic.lua
@@ -43,3 +43,8 @@ function printHiddenStats()
     print("Melee Hit Chance " .. GetHitModifier() .. "%")
     print("Spell Hit Chance " .. GetSpellHitModifier() .. "%")
 end
+
+function round(num, numDecimalPlaces)
+    local mult = 10^(numDecimalPlaces or 0)
+    return math.floor(num * mult + 0.5) / mult
+end
