e78ccc03cb20d4b882f97c1919deb0a4b213c292
Tom Reed <t.reed1104@gmail.com>
2020-06-01 01:50:48 +0100

    Removed old slash events

--- TeebsClassic.lua
+++ TeebsClassic.lua
@@ -27,19 +27,7 @@ frame:SetScript("OnEvent", function(self, event, ...)
 end)
 
 local function slashHandler(msg, editbox)
-    if msg == "all" then
-        printHiddenStats()
-    elseif msg == "melee" then
-        printMeleeStats()
-    elseif msg == "def" then
-        printDefensiveStats()
-    elseif msg == "spell" then
-        printMagicStats()
-    elseif msg == "" then
-        print("Please enter your selection: all, melee, def or spell")
-    else
-        print("Oops! Not recognised mode")
-    end
+
 end
 
 -- Create/load the addon database
@@ -130,59 +118,6 @@ end
 -- Slash Commands
 SLASH_TEEBSCLASSIC1 = "/teebs"
 SlashCmdList["TEEBSCLASSIC"] = slashHandler
-
-function printHiddenStats()
-    print("-------------------------------")
-    print("Melee Stats")
-    print("-------------------------------")
-    print("Melee Hit Chance " .. round(GetHitModifier(), 2) .. "%")
-    print("Melee Crit Chance " .. round(GetCritChance(), 2) .. "%")
-    print("-------------------------------")
-    print("Defensive Stats")
-    print("-------------------------------")
-    print("Dodge Chance " .. round(GetDodgeChance(), 2) .. "%")
-    print("Parry Chance " .. round(GetParryChance(), 2) .. "%")
-    print("Block Chance " .. round(GetBlockChance(), 2) .. "%")
-    print("-------------------------------")
-    print("Spell Stats")
-    print("-------------------------------")
-    print("Spell Hit Chance " .. round(GetSpellHitModifier(), 2) .. "%")
-    print("Spell Crit Chance (Physical) " .. round(GetSpellCritChance(1), 2) .. "%")
-    local averageSpellCrit = round((GetSpellCritChance(2) + GetSpellCritChance(3) + GetSpellCritChance(4) + GetSpellCritChance(5) + GetSpellCritChance(6) + GetSpellCritChance(7)) / 6, 2)
-    print("Spell Crit Chance (Average) " .. averageSpellCrit .. "%")
-end
-
-function printMeleeStats()
-    print("-------------------------------")
-    print("Melee Stats")
-    print("-------------------------------")
-    print("Melee Hit Chance " .. round(GetHitModifier(), 2) .. "%")
-    print("Melee Crit Chance " .. round(GetCritChance(), 2) .. "%")
-end
-
-function printDefensiveStats()
-    print("-------------------------------")
-    print("Defensive Stats")
-    print("-------------------------------")
-    print("Dodge Chance " .. round(GetDodgeChance(), 2) .. "%")
-    print("Parry Chance " .. round(GetParryChance(), 2) .. "%")
-    print("Block Chance " .. round(GetBlockChance(), 2) .. "%")
-end
-
-function printMagicStats()
-    print("-------------------------------")
-    print("Spell Stats")
-    print("-------------------------------")
-    print("Spell Hit Chance " .. round(GetSpellHitModifier(), 2) .. "%")
-    print("Spell Crit Chance (Physical) " .. round(GetSpellCritChance(1), 2) .. "%")
-    local averageSpellCrit = round((GetSpellCritChance(2) + GetSpellCritChance(3) + GetSpellCritChance(4) + GetSpellCritChance(5) + GetSpellCritChance(6) + GetSpellCritChance(7)) / 6, 2)
-    print("Spell Crit Chance (Average) " .. averageSpellCrit .. "%")
-end
-
-function round(num, numDecimalPlaces)
-    local mult = 10^(numDecimalPlaces or 0)
-    return math.floor(num * mult + 0.5) / mult
-end
 
 function getCharacterItemsID(slot)
     -- Get the base details about the logged in session
