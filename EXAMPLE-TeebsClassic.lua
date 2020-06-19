4e990b7250954d03f09708e75a0c77f4edc4fb78
Tom Reed <t.reed1104@gmail.com>
2020-06-20 00:10:09 +0100

    Restructured the backup file as an example file, with the new layout

---
+++ EXAMPLE-TeebsClassic.lua
@@ -0,0 +1,740 @@
+
+TeebsClassicDB = {
+	["realms"] = {
+		["nethergarde keep"] = {
+			["characters"] = {
+				["teebs"] = {
+					["experienceRestedPercentage"] = 0,
+					["talents"] = {
+						["assassination"] = {
+							["1"] = {
+								["1"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 3,
+									["name"] = "Improved Eviscerate",
+								},
+								["3"] = {
+									["currentRank"] = 5,
+									["maxRank"] = 5,
+									["name"] = "Malice",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 2,
+									["name"] = "Remorseless Attacks",
+								},
+							},
+							["3"] = {
+								["1"] = {
+									["currentRank"] = 1,
+									["maxRank"] = 1,
+									["name"] = "Relentless Strikes",
+								},
+								["3"] = {
+									["currentRank"] = 5,
+									["maxRank"] = 5,
+									["name"] = "Lethality",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 2,
+									["name"] = "Improved Expose Armor",
+								},
+							},
+							["2"] = {
+								["1"] = {
+									["currentRank"] = 3,
+									["maxRank"] = 3,
+									["name"] = "Ruthlessness",
+								},
+								["4"] = {
+									["currentRank"] = 3,
+									["maxRank"] = 3,
+									["name"] = "Improved Slice and Dice",
+								},
+								["2"] = {
+									["currentRank"] = 2,
+									["maxRank"] = 2,
+									["name"] = "Murder",
+								},
+							},
+							["5"] = {
+								["3"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 3,
+									["name"] = "Improved Kidney Shot",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 1,
+									["name"] = "Cold Blood",
+								},
+							},
+							["4"] = {
+								["3"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Improved Poisons",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Vile Poisons",
+								},
+							},
+							["7"] = {
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 1,
+									["name"] = "Vigor",
+								},
+							},
+							["6"] = {
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Seal Fate",
+								},
+							},
+						},
+						["combat"] = {
+							["1"] = {
+								["1"] = {
+									["currentRank"] = 3,
+									["maxRank"] = 3,
+									["name"] = "Improved Gouge",
+								},
+								["3"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Lightning Reflexes",
+								},
+								["2"] = {
+									["currentRank"] = 2,
+									["maxRank"] = 2,
+									["name"] = "Improved Sinister Strike",
+								},
+							},
+							["3"] = {
+								["1"] = {
+									["currentRank"] = 2,
+									["maxRank"] = 2,
+									["name"] = "Endurance",
+								},
+								["4"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 2,
+									["name"] = "Improved Sprint",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 1,
+									["name"] = "Riposte",
+								},
+							},
+							["2"] = {
+								["1"] = {
+									["currentRank"] = 3,
+									["maxRank"] = 3,
+									["name"] = "Improved Backstab",
+								},
+								["3"] = {
+									["currentRank"] = 5,
+									["maxRank"] = 5,
+									["name"] = "Precision",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Deflection",
+								},
+							},
+							["5"] = {
+								["1"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Mace Specialization",
+								},
+								["4"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Fist Weapon Specialization",
+								},
+								["3"] = {
+									["currentRank"] = 5,
+									["maxRank"] = 5,
+									["name"] = "Sword Specialization",
+								},
+								["2"] = {
+									["currentRank"] = 1,
+									["maxRank"] = 1,
+									["name"] = "Blade Flurry",
+								},
+							},
+							["4"] = {
+								["1"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 2,
+									["name"] = "Improved Kick",
+								},
+								["3"] = {
+									["currentRank"] = 5,
+									["maxRank"] = 5,
+									["name"] = "Dual Wield Specialization",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Dagger Specialization",
+								},
+							},
+							["7"] = {
+								["2"] = {
+									["currentRank"] = 1,
+									["maxRank"] = 1,
+									["name"] = "Adrenaline Rush",
+								},
+							},
+							["6"] = {
+								["3"] = {
+									["currentRank"] = 3,
+									["maxRank"] = 3,
+									["name"] = "Aggression",
+								},
+								["2"] = {
+									["currentRank"] = 2,
+									["maxRank"] = 2,
+									["name"] = "Weapon Expertise",
+								},
+							},
+						},
+						["subtlety"] = {
+							["1"] = {
+								["3"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Opportunity",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Master of Deception",
+								},
+							},
+							["3"] = {
+								["1"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 3,
+									["name"] = "Initiative",
+								},
+								["3"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 3,
+									["name"] = "Improved Ambush",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 1,
+									["name"] = "Ghostly Strike",
+								},
+							},
+							["2"] = {
+								["1"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 2,
+									["name"] = "Sleight of Hand",
+								},
+								["3"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Camouflage",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 2,
+									["name"] = "Elusiveness",
+								},
+							},
+							["5"] = {
+								["1"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 2,
+									["name"] = "Heightened Senses",
+								},
+								["4"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 1,
+									["name"] = "Hemorrhage",
+								},
+								["3"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 2,
+									["name"] = "Dirty Deeds",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 1,
+									["name"] = "Preparation",
+								},
+							},
+							["4"] = {
+								["1"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 3,
+									["name"] = "Setup",
+								},
+								["3"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 3,
+									["name"] = "Serrated Blades",
+								},
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 3,
+									["name"] = "Improved Sap",
+								},
+							},
+							["7"] = {
+								["2"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 1,
+									["name"] = "Premeditation",
+								},
+							},
+							["6"] = {
+								["3"] = {
+									["currentRank"] = 0,
+									["maxRank"] = 5,
+									["name"] = "Deadliness",
+								},
+							},
+						},
+						["specialisation"] = "19/32/0",
+					},
+					["class"] = "Rogue",
+					["currency"] = {
+						["silver"] = 27,
+						["copper"] = 17,
+						["gold"] = 111,
+					},
+					["bags"] = {
+						["1"] = 17966,
+						["4"] = 14155,
+						["3"] = 14155,
+						["2"] = 4500,
+					},
+					["faction"] = "Alliance",
+					["gear"] = {
+						["5"] = 16905,
+						["4"] = 0,
+						["14"] = 13965,
+						["15"] = 13340,
+						["1"] = 16908,
+						["0"] = 18042,
+						["3"] = 16832,
+						["2"] = 18404,
+						["19"] = 0,
+						["18"] = 17069,
+						["7"] = 16909,
+						["6"] = 16910,
+						["9"] = 16911,
+						["8"] = 16906,
+						["16"] = 19352,
+						["17"] = 19866,
+						["13"] = 11815,
+						["12"] = 18500,
+						["11"] = 19384,
+						["10"] = 16907,
+					},
+					["level"] = 60,
+					["professions"] = {
+						["primary"] = {
+							["Herbalism"] = {
+								["max"] = 300,
+								["current"] = 300,
+							},
+							["Enchanting"] = {
+								["max"] = 300,
+								["current"] = 300,
+							},
+						},
+						["secondary"] = {
+							["Cooking"] = {
+								["max"] = 150,
+								["current"] = 140,
+							},
+							["First Aid"] = {
+								["max"] = 300,
+								["current"] = 300,
+							},
+							["Fishing"] = {
+								["max"] = 75,
+								["current"] = 1,
+							},
+						},
+					},
+					["experienceCurrentPercentage"] = 0,
+				},
+				["tweebs"] = {
+					["experienceRestedPercentage"] = 0,
+					["talents"] = {
+						["demonology"] = {
+							["1"] = {
+								["1"] = {
+									["maxRank"] = 2,
+									["name"] = "Improved Healthstone",
+									["currentRank"] = 1,
+								},
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Demonic Embrace",
+									["currentRank"] = 5,
+								},
+								["2"] = {
+									["maxRank"] = 3,
+									["name"] = "Improved Imp",
+									["currentRank"] = 0,
+								},
+							},
+							["3"] = {
+								["1"] = {
+									["maxRank"] = 3,
+									["name"] = "Improved Succubus",
+									["currentRank"] = 2,
+								},
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Fel Stamina",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 1,
+									["name"] = "Fel Domination",
+									["currentRank"] = 0,
+								},
+							},
+							["2"] = {
+								["1"] = {
+									["maxRank"] = 2,
+									["name"] = "Improved Health Funnel",
+									["currentRank"] = 0,
+								},
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Fel Intellect",
+									["currentRank"] = 5,
+								},
+								["2"] = {
+									["maxRank"] = 3,
+									["name"] = "Improved Voidwalker",
+									["currentRank"] = 0,
+								},
+							},
+							["5"] = {
+								["1"] = {
+									["maxRank"] = 5,
+									["name"] = "Improved Enslave Demon",
+									["currentRank"] = 0,
+								},
+								["4"] = {
+									["maxRank"] = 2,
+									["name"] = "Improved Firestone",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 1,
+									["name"] = "Demonic Sacrifice",
+									["currentRank"] = 0,
+								},
+							},
+							["4"] = {
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Unholy Power",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 2,
+									["name"] = "Master Summoner",
+									["currentRank"] = 0,
+								},
+							},
+							["7"] = {
+								["3"] = {
+									["maxRank"] = 2,
+									["name"] = "Improved Spellstone",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 1,
+									["name"] = "Soul Link",
+									["currentRank"] = 0,
+								},
+							},
+							["6"] = {
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Master Demonologist",
+									["currentRank"] = 0,
+								},
+							},
+						},
+						["destruction"] = {
+							["1"] = {
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Cataclysm",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 5,
+									["name"] = "Improved Shadow Bolt",
+									["currentRank"] = 0,
+								},
+							},
+							["3"] = {
+								["1"] = {
+									["maxRank"] = 2,
+									["name"] = "Improved Firebolt",
+									["currentRank"] = 0,
+								},
+								["4"] = {
+									["maxRank"] = 1,
+									["name"] = "Shadowburn",
+									["currentRank"] = 0,
+								},
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Devastation",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 2,
+									["name"] = "Improved Lash of Pain",
+									["currentRank"] = 0,
+								},
+							},
+							["2"] = {
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Aftermath",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 5,
+									["name"] = "Bane",
+									["currentRank"] = 0,
+								},
+							},
+							["5"] = {
+								["1"] = {
+									["maxRank"] = 2,
+									["name"] = "Pyroclasm",
+									["currentRank"] = 0,
+								},
+								["3"] = {
+									["maxRank"] = 1,
+									["name"] = "Ruin",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 5,
+									["name"] = "Improved Immolate",
+									["currentRank"] = 0,
+								},
+							},
+							["4"] = {
+								["1"] = {
+									["maxRank"] = 2,
+									["name"] = "Intensity",
+									["currentRank"] = 0,
+								},
+								["4"] = {
+									["maxRank"] = 5,
+									["name"] = "Improved Searing Pain",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 2,
+									["name"] = "Destructive Reach",
+									["currentRank"] = 0,
+								},
+							},
+							["7"] = {
+								["2"] = {
+									["maxRank"] = 1,
+									["name"] = "Conflagrate",
+									["currentRank"] = 0,
+								},
+							},
+							["6"] = {
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Emberstorm",
+									["currentRank"] = 0,
+								},
+							},
+						},
+						["affliction"] = {
+							["1"] = {
+								["3"] = {
+									["maxRank"] = 5,
+									["name"] = "Improved Corruption",
+									["currentRank"] = 5,
+								},
+								["2"] = {
+									["maxRank"] = 5,
+									["name"] = "Suppression",
+									["currentRank"] = 3,
+								},
+							},
+							["3"] = {
+								["1"] = {
+									["maxRank"] = 3,
+									["name"] = "Improved Curse of Agony",
+									["currentRank"] = 0,
+								},
+								["3"] = {
+									["maxRank"] = 1,
+									["name"] = "Amplify Curse",
+									["currentRank"] = 1,
+								},
+								["2"] = {
+									["maxRank"] = 5,
+									["name"] = "Fel Concentration",
+									["currentRank"] = 5,
+								},
+							},
+							["2"] = {
+								["1"] = {
+									["maxRank"] = 3,
+									["name"] = "Improved Curse of Weakness",
+									["currentRank"] = 0,
+								},
+								["4"] = {
+									["maxRank"] = 5,
+									["name"] = "Improved Drain Life",
+									["currentRank"] = 5,
+								},
+								["3"] = {
+									["maxRank"] = 2,
+									["name"] = "Improved Life Tap",
+									["currentRank"] = 2,
+								},
+								["2"] = {
+									["maxRank"] = 2,
+									["name"] = "Improved Drain Soul",
+									["currentRank"] = 0,
+								},
+							},
+							["5"] = {
+								["4"] = {
+									["maxRank"] = 4,
+									["name"] = "Improved Curse of Exhaustion",
+									["currentRank"] = 0,
+								},
+								["3"] = {
+									["maxRank"] = 1,
+									["name"] = "Curse of Exhaustion",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 1,
+									["name"] = "Siphon Life",
+									["currentRank"] = 1,
+								},
+							},
+							["4"] = {
+								["1"] = {
+									["maxRank"] = 2,
+									["name"] = "Grim Reach",
+									["currentRank"] = 2,
+								},
+								["4"] = {
+									["maxRank"] = 2,
+									["name"] = "Improved Drain Mana",
+									["currentRank"] = 0,
+								},
+								["2"] = {
+									["maxRank"] = 2,
+									["name"] = "Nightfall",
+									["currentRank"] = 2,
+								},
+							},
+							["7"] = {
+								["2"] = {
+									["maxRank"] = 1,
+									["name"] = "Dark Pact",
+									["currentRank"] = 1,
+								},
+							},
+							["6"] = {
+								["2"] = {
+									["maxRank"] = 5,
+									["name"] = "Shadow Mastery",
+									["currentRank"] = 5,
+								},
+							},
+						},
+						["specialisation"] = "32/13/0",
+					},
+					["class"] = "Warlock",
+					["currency"] = {
+						["copper"] = 88,
+						["silver"] = 3,
+						["gold"] = 95,
+					},
+					["experienceCurrentPercentage"] = 60,
+					["faction"] = "Alliance",
+					["gear"] = {
+						["19"] = 5976,
+						["18"] = 15280,
+						["8"] = 10179,
+						["9"] = 14448,
+						["1"] = 10025,
+						["0"] = 0,
+						["3"] = 10063,
+						["2"] = 7731,
+						["5"] = 10021,
+						["4"] = 6097,
+						["7"] = 7519,
+						["6"] = 13144,
+						["14"] = 1490,
+						["15"] = 10231,
+						["16"] = 20536,
+						["17"] = 0,
+						["13"] = 2820,
+						["12"] = 5011,
+						["11"] = 5009,
+						["10"] = 13863,
+					},
+					["level"] = 54,
+					["professions"] = {
+						["secondary"] = {
+							["First Aid"] = {
+								["max"] = 300,
+								["current"] = 300,
+							},
+						},
+						["primary"] = {
+							["Alchemy"] = {
+								["max"] = 300,
+								["current"] = 300,
+							},
+							["Mining"] = {
+								["max"] = 150,
+								["current"] = 109,
+							},
+						},
+					},
+					["bags"] = {
+						["1"] = 11324,
+						["4"] = 14046,
+						["3"] = 14046,
+						["2"] = 14046,
+					},
+				},
+			},
+		},
+	},
+}
