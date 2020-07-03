36a21324d37996df9bf0a5f3d559dbc50cc7e58a
Tom Reed <t.reed1104@gmail.com>
2020-07-04 00:55:38 +0100

    Registed the TIME_PLAYED_MSG callback event

--- TeebsClassic.lua
+++ TeebsClassic.lua
@@ -25,6 +25,7 @@ frame:RegisterEvent("PLAYER_XP_UPDATE")
 frame:RegisterEvent("TRADE_SKILL_UPDATE")
 frame:RegisterEvent("CHARACTER_POINTS_CHANGED")
 frame:RegisterEvent("UPDATE_FACTION")
+frame:RegisterEvent("TIME_PLAYED_MSG")
 
 -- Handle Events Triggering
 frame:SetScript("OnEvent", function(self, event, arg1, arg2, arg3, ...)
