1d2bc05e0f5e3f420558e9b9849e6c66eee14fae
Tom Reed <t.reed1104@gmail.com>
2020-07-14 23:37:18 +0100

    Function bases for all single value UI functions

--- TeebsClassic.lua
+++ TeebsClassic.lua
@@ -1071,3 +1071,51 @@ end
 ------------------------------------------------------------------
 -- Addon UI Functions - Return data for binding to UI panels
 ------------------------------------------------------------------
+function interfaceGetAllItemSlots()
+    return nil
+end
+
+function interfaceGetAllBagSlots()
+    return nil
+end
+
+function interfaceGetExperience(character)
+    return nil
+end
+
+function interfaceGetLevel(character)
+    return nil
+end
+
+function interfaceGetPlayTime(character)
+    return nil
+end
+
+function interfaceGetSpec(character)
+    return nil
+end
+
+function interfaceGetTalents(character)
+    return nil
+end
+
+function interfaceGetGold(character)
+    return nil
+end
+
+function interfaceGetProfessions(character)
+    return nil
+end
+
+function interfaceGetPrimaryProfessions(character)
+    return nil
+end
+
+function interfaceGetSecondaryProfessions(character)
+    return nil
+end
+
+function interfaceGetReputation(character)
+    return nil
+end
+
