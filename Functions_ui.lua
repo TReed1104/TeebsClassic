ecdbe0ffd7a0326bc5bbe96e08c55b66a8d8b70c
Tom Reed <t.reed1104@gmail.com>
2020-07-14 23:54:29 +0100

    Copied the ui functions to their own source file

---
+++ Functions_ui.lua
@@ -0,0 +1,90 @@
+------------------------------------------------------------------
+-- Addon UI Functions - Return data for binding to UI panels
+------------------------------------------------------------------
+function interfaceGetAllItemSlots()
+    return nil
+end
+
+function interfaceGetAllBagSlots()
+    return nil
+end
+
+function interfaceGetExperience(character)
+    return nil
+end
+
+function interfaceGetLevel(character)
+    return nil
+end
+
+function interfaceGetPlayTime(character)
+    return nil
+end
+
+function interfaceGetSpec(character)
+    return nil
+end
+
+function interfaceGetTalents(character)
+    return nil
+end
+
+function interfaceGetGold(character)
+    return nil
+end
+
+function interfaceGetProfessions(character)
+    return nil
+end
+
+function interfaceGetPrimaryProfessions(character)
+    return nil
+end
+
+function interfaceGetSecondaryProfessions(character)
+    return nil
+end
+
+function interfaceGetReputation(character)
+    return nil
+end
+
+function interfaceGetAllExperience()
+    return nil
+end
+
+function interfaceGetAllLevels()
+    return nil
+end
+
+function interfaceGetAllPlayTime()
+    return nil
+end
+
+function interfaceGetAllSpecs()
+    return nil
+end
+
+function interfaceGetAllTalents()
+    return nil
+end
+
+function interfaceGetAllGold()
+    return nil
+end
+
+function interfaceGetAllProfessions()
+    return nil
+end
+
+function interfaceGetAllPrimaryProfessions()
+    return nil
+end
+
+function interfaceGetAllSecondaryProfessions()
+    return nil
+end
+
+function interfaceGetAllReputation()
+    return nil
+end
