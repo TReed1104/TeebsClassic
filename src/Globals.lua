1a31b33e8cd20494f0183a4c3306e69c116308cb
Tom Reed <t.reed1104@gmail.com>
2020-08-04 00:35:07 +0100

    Source code file restructure - renames and src folder

---
+++ src/Globals.lua
@@ -0,0 +1,9 @@
+------------------------------------------------------------------
+-- Globals Variables
+------------------------------------------------------------------
+_G["CURRENT_REALM"] = ""
+_G["CURRENT_CHARACTER_NAME"] = ""
+_G["TEEBS_TEXT_COLOUR_DEFAULT"] = "ffffff00"
+_G["TEEBS_TEXT_COLOUR_ALERT"] = "ffff0000"
+_G["TEEBS_TEXT_COLOUR_CAPPED"] = "ff00ff00"
+_G["TEEBS_TEXT_COLOUR_TALENTS"] = "ffffa500"
