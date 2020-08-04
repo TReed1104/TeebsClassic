70c10726cc53db14effe47f67afad7c678a88c32
Tom Reed <t.reed1104@gmail.com>
2020-08-05 00:28:40 +0100

    Passed the OnLoad and EventHandler to the CreateFrame call

--- TeebsClassic.lua
+++ TeebsClassic.lua
@@ -2,7 +2,7 @@
 -- Addon Core Creation, Event Registration And Handling
 ------------------------------------------------------------------
 -- Addon Initialisation and UI setup
-local TeebsClassicFrame = TeebsClassic_CreateFrame("TeebsClassic", UIParent, 500, 500, "CENTER", 0, 0, { bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background", edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border", edgeSize = 12, tile = true, tileSize = 12, insets = { left = 0, right = 0, top = 0, bottom = 0, }, })
+local TeebsClassicFrame = TeebsClassic_CreateFrame("TeebsClassic", UIParent, 500, 500, "CENTER", 0, 0, { bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background", edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border", edgeSize = 12, tile = true, tileSize = 12, insets = { left = 0, right = 0, top = 0, bottom = 0, }, }, TeebsClassic_OnLoad, TeebsClassic_EventHandler)
 ActivateLayout_Core(TeebsClassicFrame)
 Layout_MainMenu(TeebsClassicFrame)
 TeebsClassicFrame:Hide()    -- Hide the addon frame by default, Comment this out to show the addon frame on reload
