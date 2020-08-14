436fb21f8627a2aebbe36479edff04b11708f10a
Tom Reed <t.reed1104@gmail.com>
2020-08-15 00:19:16 +0100

    Moved the core eventHandler to a different source file

--- src/ui/ui_events.lua
+++ src/ui/ui_events.lua
@@ -27,6 +27,76 @@ function onLoadCoreFrame(frame, eventHandlerFunc)
     frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
 end
 
+-- The master event handler
+function eventHandler(self, event, arg1, arg2, arg3, ...)
+    -- Addon Loaded Trigger
+    if event == "ADDON_LOADED" then
+        CURRENT_REALM = GetRealmName():lower()
+        CURRENT_CHARACTER_NAME = UnitName("player"):lower()
+        initialiseDB()
+    end
+
+    -- Player Login Event Trigger
+    if event == "PLAYER_LOGIN" then
+        print("Welcome to Teebs Classic, use /teebs")
+    end
+
+    -- Player Enter World Trigger
+    if event == "PLAYER_ENTERING_WORLD" then
+        setCharacterDataItemSlots()
+        setCharacterDataCurrency()
+        setCharacterDataCharacterExperience()
+        setCharacterDataLevel()
+        setCharacterDataSpecialisation()
+        setCharacterDataProfessions()
+        setCharacterDataReputation()
+        RequestTimePlayed();    -- Send the request to the server to query the character play time, the return data happens in the TIME_PLAYED_MSG event
+    end
+
+    -- Player Level Up Trigger
+    if event == "PLAYER_LEVEL_UP" then
+        setCharacterDataCharacterExperience()
+        setCharacterDataLevel()
+        RequestTimePlayed();    -- Send the request to the server to query the character play time, the return data happens in the TIME_PLAYED_MSG event
+    end
+
+    -- Player Skill Update Trigger
+    if event == "TRADE_SKILL_UPDATE" then
+        setCharacterDataProfessions()
+    end
+
+    -- Player Current Change Trigger
+    if event == "PLAYER_MONEY" then
+        setCharacterDataCurrency()
+    end
+
+    -- Player Gear Change Trigger
+    if event == "PLAYER_EQUIPMENT_CHANGED" then
+        setCharacterDataItemSlots()
+    end
+
+    -- Player XP value update - Quest/NPC Kill
+    if event == "PLAYER_XP_UPDATE" then
+        setCharacterDataCharacterExperience()
+        setCharacterDataLevel()
+    end
+
+    -- Player Talent Changes
+    if event == "CHARACTER_POINTS_CHANGED" then
+        setCharacterDataSpecialisation()
+    end
+
+    -- Faction reputation change event
+    if event == "UPDATE_FACTION" then
+        setCharacterDataReputation()
+    end
+
+    -- Play Time callback catch
+    if event == "TIME_PLAYED_MSG" then
+        setCharacterDataPlayTime(arg1, arg2)
+    end
+end
+
 -- Click event for the exit button
 function exitButton()
     TEEBS_CLASSIC_FRAME:Hide()
