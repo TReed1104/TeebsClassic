9c2707f9cf4c38df3f1d672464e9f0e047a134c9
Tom Reed <t.reed1104@gmail.com>
2020-08-16 01:31:28 +0100

    Removed generateCoreUIElements() call

--- TeebsClassic.lua
+++ TeebsClassic.lua
@@ -3,5 +3,4 @@
 ------------------------------------------------------------------
 -- Addon Initialisation and UI setup
 TEEBS_CLASSIC_FRAME = createFrame("TeebsClassic", UIParent, 500, 500, "CENTER", 0, 0, TEEBS_CLASSIC_BACKDROP_OBJECT, onLoadCoreFrame, eventHandlerCoreFrame)
-generateCoreUIElements(TEEBS_CLASSIC_FRAME)
 TEEBS_CLASSIC_FRAME:Hide()    -- Hide the addon frame by default, Comment this out to show the addon frame on reload
