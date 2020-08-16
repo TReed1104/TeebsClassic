7eab924c11790ee513a130a251b5dfcf0ae6eac1
Tom Reed <t.reed1104@gmail.com>
2020-08-17 00:34:09 +0100

    Comment amendments

--- TeebsClassic.lua
+++ TeebsClassic.lua
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------
--- Addon Core Creation, Event Registration And Handling
+-- Addon Core
 ------------------------------------------------------------------
--- Addon Initialisation and UI setup
+-- Addon Core Frame Creation
 TEEBS_CLASSIC_FRAME = createFrame("TeebsClassic", UIParent, 500, 500, "CENTER", 0, 0, TEEBS_CLASSIC_BACKDROP_OBJECT, onLoadCoreFrame, eventHandlerCoreFrame)
 TEEBS_CLASSIC_FRAME:Hide()    -- Hide the addon frame by default, Comment this out to show the addon frame on reload
