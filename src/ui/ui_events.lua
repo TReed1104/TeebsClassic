d0051708b18b9366aca46738cd2d17c719b1c67b
Tom Reed <t.reed1104@gmail.com>
2020-08-18 00:39:49 +0100

    Added backdrop colour to the title frame creation

--- src/ui/ui_events.lua
+++ src/ui/ui_events.lua
@@ -27,7 +27,7 @@ function onLoadCoreFrame(frame, eventHandlerFunc)
     frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
 
     -- Generate the core UI elements
-    frame.titleRegion = createFrame("TitleRegion", frame, 150, 24, "TOP", 0, -2, TEEBS_CLASSIC_BACKDROP_OBJECT)
+    frame.titleRegion = createFrame("TitleRegion", frame, 150, 24, "TOP", 0, -2, TEEBS_CLASSIC_BACKDROP_OBJECT, { r = 1, g = 0, b = 0, a = 1 })
     frame.titleRegion.titleText = createTextObject("TitleText", frame.titleRegion, "TeebsClassic", 1, 1, 1, 1, "CENTER", 0, 0, "GameFontNormal")      -- Title Text
     frame.exitButton = createButton("CloseButton", frame, "x", 30, 30, "TOPRIGHT", -6, -2, exitButton)          -- Create the Exit button
     frame.menuButton = createButton("MenuButton", frame, "Menu", 50, 30, "TOPLEFT", 6, -2, openMainMenu)        -- Menu Button
