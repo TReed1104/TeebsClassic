8bc88894631c8501ed5bdea02f805dc46be4d217
Tom Reed <t.reed1104@gmail.com>
2020-08-19 00:04:28 +0100

    Moved the frame creations to use the global object colours instead of raw data

--- TeebsClassic.lua
+++ TeebsClassic.lua
@@ -2,5 +2,5 @@
 -- Addon Core
 ------------------------------------------------------------------
 -- Addon Core Frame Creation
-TEEBS_CLASSIC_FRAME = createFrame("TeebsClassic", UIParent, 500, 500, "CENTER", 0, 0, TEEBS_CLASSIC_BACKDROP_OBJECT, { r = 0, g = 0, b = 0, a = 1 }, onLoadCoreFrame, eventHandlerCoreFrame)
+TEEBS_CLASSIC_FRAME = createFrame("TeebsClassic", UIParent, 500, 500, "CENTER", 0, 0, TEEBS_CLASSIC_BACKDROP_OBJECT, TEEBS_OBJECT_COLOUR_BLACK, onLoadCoreFrame, eventHandlerCoreFrame)
 TEEBS_CLASSIC_FRAME:Hide()    -- Hide the addon frame by default, Comment this out to show the addon frame on reload
