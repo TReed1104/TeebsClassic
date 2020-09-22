6c45a6dab8b7f964bbc2dc521bd5a4f8561124a7
Tom Reed <t.reed1104@gmail.com>
2020-09-22 01:17:36 +0100

    Amended return data - nested table not needed

--- src/ui/ui_functions.lua
+++ src/ui/ui_functions.lua
@@ -364,9 +364,7 @@ function interfaceGetAllSecondaryProfessions()
     local allCharactersSecondaryProfessions = {}
     -- For every character cached for the current realm
     for characterName, characterData in pairs(TeebsClassicDB.realms[CURRENT_REALM].characters) do
-        allCharactersSecondaryProfessions[characterName] = {
-            professions = characterData.professions["secondary"]
-        }
+        allCharactersSecondaryProfessions[characterName] = characterData.professions["secondary"]
     end
     -- Return our collated character professions
     return allCharactersSecondaryProfessions
